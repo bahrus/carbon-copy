3d56dd1a91237c1a15d79935e1852abe1ce357a3
bahrus <anderson.bruce.b@gmail.com>
2017-10-03 21:23:39 -0400

    cc_resolver

--- demo/cc_resolver.ts
+++ demo/cc_resolver.ts
@@ -8,12 +8,27 @@ export interface ICarbonCopy{
 function cc_resolver(doc: Document | HTMLTemplateElement, cc: ICarbonCopy){
     const target = doc['body'] || doc['content'];
     cc.qsa('template', target).forEach((template: HTMLTemplateElement) =>{
-        cc.qsa('c-c[href],carbon-copy[href],link[rel="import"]', template.content).forEach(hrefTag =>{
-            const href = hrefTag.getAttribute('href');
-            const splitHref= href.split('#');
-            const newHref = cc.absolute(cc._absUrl, splitHref[0]);
-            hrefTag.setAttribute('href', newHref + '#' + splitHref[1]);
-            cc_resolver(template, cc);
+        cc.qsa('c-c[href],carbon-copy[href],link[rel="import"],link[rel="stylesheet"],script,iframe', template.content).forEach(hrefOrScriptTag =>{
+            switch(hrefOrScriptTag.tagName){
+                case 'IFRAME':
+                case 'SCRIPT':
+                    const src = hrefOrScriptTag.getAttribute('src');
+                    const newSrc = cc.absolute(cc._absUrl, src);
+                    hrefOrScriptTag.setAttribute('src', newSrc);
+                    break;
+                default:
+                    const href = hrefOrScriptTag.getAttribute('href');
+                    if(href){
+                        const splitHref= href.split('#');
+                        const newHref = cc.absolute(cc._absUrl, splitHref[0]);
+                        hrefOrScriptTag.setAttribute('href', newHref + (splitHref.length > 0 ? ('#' + splitHref[1]) : ''));
+                    }
+            }
+
+            cc.qsa('template', template.content).forEach((childTemplate: HTMLTemplateElement) =>{
+                cc_resolver(childTemplate, cc);
+            });
+            
         })
 
     })    
