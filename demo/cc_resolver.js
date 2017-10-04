7ef38aebe1ace249b0f89ae02acc9ecaa0bcf6ac
bahrus <anderson.bruce.b@gmail.com>
2017-10-03 21:02:24 -0400

    reduce size

--- demo/cc_resolver.js
+++ demo/cc_resolver.js
@@ -1,16 +1,12 @@
 function cc_resolver(doc, cc) {
-    const templates = doc.body.querySelectorAll('template');
-    for (let i = 0, ii = templates.length; i < ii; i++) {
-        const template = templates[i];
-        const hrefs = template.content.querySelectorAll('c-c[href],carbon-copy[href],link[rel="import"]');
-        for (let j = 0, jj = hrefs.length; j < jj; j++) {
-            const hrefTag = hrefs[j];
+    cc.qsa('template', doc.body).forEach((template) => {
+        cc.qsa('c-c[href],carbon-copy[href],link[rel="import"]', template.content).forEach(hrefTag => {
             const href = hrefTag.getAttribute('href');
             const splitHref = href.split('#');
             const newHref = cc.absolute(cc._absUrl, splitHref[0]);
             hrefTag.setAttribute('href', newHref + '#' + splitHref[1]);
-        }
-    }
+        });
+    });
     return doc;
 }
 //# sourceMappingURL=cc_resolver.js.map
--- demo/cc_resolver.ts
+++ demo/cc_resolver.ts
@@ -1,17 +1,19 @@
-function cc_resolver(doc: Document, cc: any){
-    const templates = doc.body.querySelectorAll('template');
-    for(let i = 0, ii = templates.length; i < ii; i++){
-        const template = templates[i];
-        const hrefs = template.content.querySelectorAll('c-c[href],carbon-copy[href],link[rel="import"]');
-        for(let j = 0, jj = hrefs.length; j < jj; j++){
-            const hrefTag = hrefs[j];
+//TODO:  eliminate duplicity
+export interface ICarbonCopy{
+    absolute(base: string, relative: string) : string;
+    _absUrl: string;
+    qsa(css, from?: HTMLElement | Document | DocumentFragment) : HTMLElement[];
+}
+
+function cc_resolver(doc: Document, cc: ICarbonCopy){
+    cc.qsa('template', doc.body).forEach((template: HTMLTemplateElement) =>{
+        cc.qsa('c-c[href],carbon-copy[href],link[rel="import"]', template.content).forEach(hrefTag =>{
             const href = hrefTag.getAttribute('href');
             const splitHref= href.split('#');
             const newHref = cc.absolute(cc._absUrl, splitHref[0]);
             hrefTag.setAttribute('href', newHref + '#' + splitHref[1]);
-        }
+        })
 
-
-    }
+    })    
     return doc;
 }
