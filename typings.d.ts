901e2705a2ad743b424277ea5057de3aac72119f
bahrus <anderson.bruce.b@gmail.com>
2019-11-05 20:05:14 -0500

    ...

--- typings.d.ts
+++ typings.d.ts
@@ -1,7 +1,10 @@
+import { RenderContext } from "../trans-render/init.d.js";
+
 export interface BCC_WC{
     noclear: boolean;
     noshadow: boolean;
     from: string;
     viewModel: object;
     copy: boolean;
+    renderContext: RenderContext
 }
