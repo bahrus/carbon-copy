8b715650fe26b5d0bef3f287bf451b225c7d371d
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-07-23 19:41:06 -0400

    ...

--- demo/counter-fa.ts
+++ demo/counter-fa.ts
@@ -4,10 +4,10 @@ import('pass-down/p-d.js');
 import('aggregator-fn/ag-fn.js');
 
 define('counter-fa', html`
-<button part=down data-d=-1>-</button>
+<button disabled part=down data-d=-1>-</button>
 <p-d on=click to=[-d] m=1 val=target.dataset.d parse-val-as=int></p-d>
 <span part=count>{{count}}</span>
-<button part=up data-d=1>+</button>
+<button disabled part=up data-d=1>+</button>
 <p-d on=click to=[-d] m=1 val=target.dataset.d parse-val-as=int></p-d>
 <ag-fn -d><script nomodule>
     ({d, host}) => {
